487729d05e8ebb63da0447cc9386e6374b15f5e7
vipin uniyal <uniyalvipin70@gmail.com>
2021-06-20 17:27:50 +0530

    React single page application

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
     if (dish != null) {
@@ -58,12 +60,27 @@ const DishDetail = (props) => {
         return (<div></div>);
     }
 
-    const dishItem = RenderDish(dish);
-    const dishComment = RenderComments(dish.comments);
     return (
-        <div className='row'>
-            {dishItem}
-            {dishComment}
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle  } from "reactstrap";
+import { Card, CardImg, CardImgOverlay,
+  CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderMenuItem({dish, onClick}) {
+
+function RenderMenuItem ({dish, onClick}) {
   return (
-    <Card onClick={ () => onClick(dish.id)} >
-      <CardImg width="100%" src={ dish.image } alt={ dish.name } />
-      <CardImgOverlay>
-        <CardTitle> { dish.name }</CardTitle>
-      </CardImgOverlay>
-    </Card>
-  )
+      <Card>
+          <Link to={`/menu/${dish.id}`} >
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardImgOverlay>
+                  <CardTitle>{dish.name}</CardTitle>
+              </CardImgOverlay>
+          </Link>
+      </Card>
+  );
 }
 
 const Menu = props => {
@@ -23,11 +28,21 @@ const Menu = props => {
   })
 
   return (
-      <div className="container">
-          <div className="row">
-              { menu }
-          </div>
-      </div>
+    <div className="container">
+        <div className="row">
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                <BreadcrumbItem active>Menu</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>Menu</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            {menu}
+        </div>
+    </div>
   );
 }
 
